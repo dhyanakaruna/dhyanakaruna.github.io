a27772d7f224818170539636815c7427c081c202
Dhyana <dhyanakarunanidhi@gmail.com>
2025-08-12 01:11:36 +0530

    base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/dhyanakaruna.github.io/' : '/',
+  base: '/',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
